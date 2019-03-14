9531046a262de0730c2523c0c8a2c3fb155837cb
Maxime-Beaufils <thelimier@gmail.com>
2019-03-14 08:39:01 +0100

    fix shuffle bug

--- store/index.js
+++ store/index.js
@@ -23,7 +23,7 @@ const createStore = () => {
       async nuxtServerInit ({ commit }, { $axios }) {
         const movieData = await $axios.$get('https://api.themoviedb.org/3/discover/movie?api_key=6826192083758777324271fcce50a01c&?sort_by=popularity.desc&page='+Math.floor(Math.random() * 5) + 1  );
         const genreData = await $axios.$get('https://api.themoviedb.org/3/genre/movie/list?api_key=6826192083758777324271fcce50a01c&language=en-US');
-        commit('setMovies', movieData)
+        await commit('setMovies', movieData)
         commit('setGenres', genreData)
       },
       shuffleMovie({commit}){
